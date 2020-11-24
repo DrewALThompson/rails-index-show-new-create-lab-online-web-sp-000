ef5c9a205fc153739030a1c7802e128bb873349b
Drew Thompson <drewallynthompson@gmail.com>
2020-11-24 08:23:55 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -5,10 +5,14 @@ class CouponsController < ApplicationController
   end
   
   def show 
+    @coupon = Coupon.find(params[:id])
   end 
   
-  private
+  def new 
+  end 
   
-    
+  def create
+    @coupon = Coupon.create(coupon_code: params[:coupon][:coupon_code], store: params[:coupon][:store])
+  end 
   
 end 
